6710eb24f449d6100c34abe0f005eb6cffc9ccb1
Allinks <60175577+0-RTT@users.noreply.github.com>
2024-04-08 10:59:32 +0800

    Update api.php

--- api/api.php
+++ api/api.php
@@ -1,124 +1,104 @@
 <?php
 
-// 判断是否有文件上传
-if (!isset($_FILES["file"])) {
-  $result = array(
-    "code" => 201,
-    "msg" => "没有上传文件！"
-  );
-  outputResult($result);
-  exit;
+class ImageUploader
+{
+    private $allowedTypes = ["image/gif", "image/jpeg", "image/jpg", "image/pjpeg", "image/x-png", "image/png"];
+    private $maxSize = 5 * 1024 * 1024;
+    private $domains = ['img.selipoi.top','picture.atago.moe'];
+
+    public function upload()
+    {
+        if (!isset($_FILES["file"])) {
+            $this->outputResult(["code" => 201, "msg" => "没有上传文件！"]);
+            exit;
+        }
+
+        $file = $_FILES["file"]["name"];
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
+        $fileType = $_FILES["file"]["type"];
+        if (!in_array($fileType, $this->allowedTypes)) {
+            $this->outputResult(["code" => 201, "msg" => "只允许上传gif、jpeg、jpg、png格式的图片文件！"]);
+            exit;
+        }
+
+        $fileSize = $_FILES["file"]["size"];
+        if ($fileSize > $this->maxSize) {
+            $compressedImage = $this->compress_image($_FILES["file"]);
+            if (!$compressedImage) {
+                $this->outputResult(["code" => 201, "msg" => "图片压缩失败！"]);
+                exit;
+            }
+            $fileType = $compressedImage['type'];
+            $fileSize = $compressedImage['size'];
+            $filepath = $compressedImage['tmp_name'];
+        } else {
+            $filepath = $_FILES["file"]["tmp_name"];
+        }
+
+        $imgpath = $this->upload_image($filepath, $fileType, $file);
+        if ($imgpath) {
+            $image_host = 'https://'. $this->domains[array_rand($this->domains)];
+            $this->outputResult(["code" => 200, "msg" => "上传成功", "url" => $image_host . $imgpath]);
+        } else {
+            $this->outputResult(["code" => 201, "msg" => "图片上传失败！请检查接口可用性！"]);
+        }
+    }
+
+    private function compress_image($image)
+    {
+        if ($image['size'] <= $this->maxSize) {
+            return $image;
+        }
+
+        $temp_file = tempnam(sys_get_temp_dir(), 'image');
+        if (!$temp_file) {
+            return false;
+        }
+        imagejpeg(imagecreatefromstring(file_get_contents($image['tmp_name'])), $temp_file, 80);
+        $compressed_size = filesize($temp_file);
+
+        if ($compressed_size <= $this->maxSize) {
+            return [
+                'name' => $image['name'],
+                'type' => 'image/jpeg',
+                'tmp_name' => $temp_file,
+                'error' => 0,
+                'size' => $compressed_size
+            ];
+        } else {
+            unlink($temp_file);
+            return false;
+        }
+    }
+
+    private function outputResult($result)
+    {
+        header("Content-type: application/json");
+        echo json_encode($result, true);
+    }
+
+    private function upload_image($filepath, $fileType, $fileName)
+    {
+        $data = [
+            'file' => curl_file_create($filepath, $fileType, $fileName)
+        ];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://telegra.ph/upload');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $json = json_decode($response, true);
+        if ($json && isset($json[0]['src'])) {
+            return $json[0]['src'];
+        } else {
+            return false;
+        }
+    }
 }
 
-// 获取上传的文件名和扩展名
-$file = $_FILES["file"]["name"];
-$extension = pathinfo($file, PATHINFO_EXTENSION);
-
-// 判断上传的文件类型是否允许
-$fileType = $_FILES["file"]["type"];
-$allowedTypes = ["image/gif", "image/jpeg", "image/jpg", "image/pjpeg", "image/x-png", "image/png"];
-if (!in_array($fileType, $allowedTypes)) {
-  $result = array(
-    "code" => 201,
-    "msg" => "只允许上传gif、jpeg、jpg、png格式的图片文件！"
-  );
-  outputResult($result);
-  exit;
-}
-
-// 定义最大允许上传的文件大小为5MB
-$maxSize = 5 * 1024 * 1024;
-$fileSize = $_FILES["file"]["size"];
-
-// 如果上传的文件大小超过最大允许大小，则进行压缩
-if ($fileSize > $maxSize) {
-  $compressedImage = compress_image($_FILES["file"], $maxSize);
-  if (!$compressedImage) {
-    $result = array(
-      "code" => 201,
-      "msg" => "图片压缩失败！"
-    );
-    outputResult($result);
-    exit;
-  }
-  $fileType = $compressedImage['type'];
-  $fileSize = $compressedImage['size'];
-  $filepath = $compressedImage['tmp_name'];
-} else {
-  $filepath = $_FILES["file"]["tmp_name"];
-}
-
-// 调用upload_image函数上传图片
-$imgpath = upload_image($filepath, $fileType, $file);
-if ($imgpath) {
-$domains = ['img.selipoi.top','picture.atago.moe'];
-$image_host = 'https://'. $domains[array_rand($domains)];
-  $result = array(
-    "code" => 200,
-    "msg" => "上传成功",
-    "url" => $image_host . $imgpath
-  );
-} else {
-  $result = array(
-    "code" => 201,
-    "msg" => "图片上传失败！请检查接口可用性！"
-  );
-}
-
-// 输出结果
-outputResult($result);
-
-// 压缩图片函数
-function compress_image($image, $maxSize) {
-  if ($image['size'] <= $maxSize) {
-    return $image;
-  }
-
-  $temp_file = tempnam(sys_get_temp_dir(), 'image');
-  if (!$temp_file) {
-    return false;
-  }
-  imagejpeg(imagecreatefromstring(file_get_contents($image['tmp_name'])), $temp_file, 80);
-  $compressed_size = filesize($temp_file);
-
-  if ($compressed_size <= $maxSize) {
-    return array(
-      'name' => $image['name'],
-      'type' => 'image/jpeg',
-      'tmp_name' => $temp_file,
-      'error' => 0,
-      'size' => $compressed_size
-    );
-  } else {
-    unlink($temp_file);
-    return false;
-  }
-}
-
-// 输出结果函数
-function outputResult($result) {
-  header("Content-type: application/json");
-  echo json_encode($result, true);
-}
-
-// 上传图片函数
-function upload_image($filepath, $fileType, $fileName) {
-  $data = array(
-    'file' => curl_file_create($filepath, $fileType, $fileName)
-  );
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, 'https://telegra.ph/upload');
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-  $response = curl_exec($ch);
-  curl_close($ch);
-
-  $json = json_decode($response, true);
-  if ($json && isset($json[0]['src'])) {
-    return $json[0]['src'];
-  } else {
-    return false;
-  }
-}
+$uploader = new ImageUploader();
+$uploader->upload();
